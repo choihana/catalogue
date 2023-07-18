0e89d04501a153c5aef85ae9cfa4d68924f64184
Joey <hanazzang999@gmail.com>
2023-07-18 10:09:27 +0900

    model manager add

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF' , 'Draft'
@@ -22,7 +28,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
-
+    objects = models.Manager() #기본 매니저
+    published = PublishedManager() #사용자 정의 매니저
     class Meta:
         ordering = ['-publish']
         indexes = [
